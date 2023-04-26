9893656186d8d4fb418436002a081738c667d880
Chernet Balcha <chernetbala@gmail.com>
2023-04-26 10:21:53 -0500

    admin first commit

---
+++ src/main/java/com/waa/PropertyManagment/PropertyManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.waa.PropertyManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyManagmentApplication.class, args);
+	}
+
+}
